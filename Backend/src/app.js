d34b96576c2793b8c02757f82caa7027b1220181
unknown <sera.shine@kalvium.community>
2025-01-13 15:02:41 +0530

    milestone 13 and 14

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@
 const express=require('express');
 const userRouter=require('./routes/user.route.js')
 const productRouter=require('./routes/product.route.js');
+const cors=require("cors");
 if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config({
         path: './src/config/.env',
@@ -14,7 +15,7 @@ const app=express();
 
 
 app.use(express.json())
-
+app.use(cors({origin:['http://localhost:5173','http://localhost:8080']}));
 app.get('/',(req,res)=>{
     return res.send('heyy there!')
 })
--- Backend/src/routes/product.route.js
+++ Backend/src/routes/product.route.js
@@ -1,7 +1,7 @@
 const multer=require('multer');
 const upload = multer({ dest: 'temp-uploads/' });
 const express = require('express');
-const {createProductController,getProductDataController} = require('../controllers/product.controller.js');
+const {createProductController,getProductDataController,updateProductController,getSingleProductDocumentController,deleteSingleProduct} = require('../controllers/product.controller.js');
 const router = express.Router();
 
 router.post(
@@ -10,5 +10,7 @@ router.post(
   createProductController
 );
 router.get('/get-products',getProductDataController)
-
+router.put('/update-products/:id',upload.array('files',5),updateProductController)
+router.get('/get-single/:id',getSingleProductDocumentController);
+router.delete('/:id',deleteSingleProduct)
 module.exports = router;

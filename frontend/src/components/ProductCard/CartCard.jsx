b32fc8410572982320b0c66b7603d46811bf0b51
unknown <sera.shine@kalvium.community>
2025-01-31 11:57:20 +0530

    milestone 24

--- frontend/src/components/ProductCard/CartCard.jsx
+++ frontend/src/components/ProductCard/CartCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { X, RotateCcw } from 'lucide-react';
 export default function CartCard({
   title,
-  image,
+  images,
   Index,
   description,
   originalPrice,
@@ -18,7 +18,7 @@ export default function CartCard({
         <div className="relative w-64">
           <Link to={`/product-details/${id}`}>
             <img
-              src={image}
+              src={typeof images=='string'?images:images[0]}
               alt="Product Image"
               className="rounded-lg object-cover"
             />
